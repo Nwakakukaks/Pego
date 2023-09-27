fecc8dfda06dbfe44d777283618d120f7b596e81
nwakakukaks <winnernwakaku123@gmail.com>
2023-09-27 04:11:31 +0100

    app setup

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/mui-overrides.scss';
+import '../styles/globals.scss';
+import '../styles/appmain.scss';
+import '../styles/index.scss';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from 'theme/theme';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
+
+export default MyApp;
