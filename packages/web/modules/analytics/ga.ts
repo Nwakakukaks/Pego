d03d784855c0a7cc0d9fce4115c478806304d918
nwakakukaks <winnernwakaku123@gmail.com>
2023-09-29 16:29:31 +0100

    fixed dep. issue

--- packages/web/modules/analytics/ga.ts
+++ packages/web/modules/analytics/ga.ts
@@ -2,18 +2,13 @@ import { Config } from '@core/config/config';
 import { Environments } from '@core/enums/environments';
 
 export const getGATrackingId = (): string => {
-  const optimismTrackingId = 'G-W7FG9D1W1X';
-  const baseTrackingId = 'G-4BVFN4FPYH';
-  const zoraTrackingId = 'G-4BVFN4FPYH';
-  const modeTrackingId = 'G-W7FG9D1W1X';
+  const PegoMainnetTrackingId = 'G-W7FG9D1W1X';
+  const PegoTestnetTrackingId = 'G-4BVFN4FPYH';
+  
 
-  return Config.environment === Environments.Optimism
-    ? optimismTrackingId
-    : Config.environment === Environments.Base
-    ? baseTrackingId
-    : Config.environment === Environments.Zora
-    ? zoraTrackingId
-    : Config.environment === Environments.Mode
-    ? modeTrackingId
+  return Config.environment === Environments.PegoMainnet
+    ? PegoMainnetTrackingId
+    : Config.environment === Environments.PegoTestnet
+    ? PegoTestnetTrackingId
     : '';
 };
