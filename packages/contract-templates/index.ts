c016f88fe6593b1ff803454cfff71a011eed63f4
nwakakukaks <winnernwakaku123@gmail.com>
2023-10-06 12:09:59 +0100

    final push

--- packages/contract-templates/index.ts
+++ packages/contract-templates/index.ts
@@ -1,11 +1,11 @@
 import templateCrowdSale from './templates/CrowdSale';
-import templateERC20 from './templates/ZRC20';
-import templateERC721 from './templates/ZRC721';
+import templateZRC20 from './templates/ZRC20';
+import templateZRC721 from './templates/ZRC721';
 import templateOneTimeSubscription from './templates/Subscription';
 
 export const TEMPLATES = [
-  templateERC20,
-  templateERC721,
+  templateZRC20,
+  templateZRC721,
   templateCrowdSale,
   templateOneTimeSubscription,
 ];
