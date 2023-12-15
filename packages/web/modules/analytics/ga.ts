8eaca6fbf2be57e997da80ca8d05e6579912dec5
nwakakukaks <winnernwakaku123@gmail.com>
2023-12-15 16:52:03 +0100

    deploy err fixed

--- packages/web/modules/analytics/ga.ts
+++ packages/web/modules/analytics/ga.ts
@@ -2,13 +2,13 @@ import { Config } from '@core/config/config';
 import { Environments } from '@core/enums/environments';
 
 export const getGATrackingId = (): string => {
-  const ZetaMainnetTrackingId = 'G-W7FG9D1W1X';
-  const ZetaTestnetTrackingId = 'G-4BVFN4FPYH';
+  const PegoMainnetTrackingId = 'G-W7FG9D1W1X';
+  const PegoTestnetTrackingId = 'G-4BVFN4FPYH';
   
 
-  return Config.environment === Environments.ZetaMainnet
-    ? ZetaMainnetTrackingId
-    : Config.environment === Environments.ZetaTestnet
-    ? ZetaTestnetTrackingId
+  return Config.environment === Environments.PegoMainnet
+    ? PegoMainnetTrackingId
+    : Config.environment === Environments.PegoTestnet
+    ? PegoTestnetTrackingId
     : '';
 };
